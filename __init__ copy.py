ffa3c54aa5aaa983ebc382fcbdb62ba9a6d79d1e
zp0217 <pzun0217@gmail.com>
2025-09-18 19:22:05 -0400

    testing forbranch-upload file

---
+++ __init__ copy.py
@@ -0,0 +1,6 @@
+"""Mailer package: simple utilities to compose and send email alerts.
+
+The package exposes high-level alert helpers in :mod:`mailer.alert` and low-level
+message helpers in :mod:`mailer.mailer`.
+"""
+__all__ = ["mailer"]
